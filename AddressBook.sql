45933cfc1c4f5f1e42438a9d561cbedf9be47e1d
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-21 09:34:51 +0530

    [Swapnil] Added UC3 Add New Conatact

--- AddressBook.sql
+++ AddressBook.sql
@@ -12,3 +12,7 @@ Zip bigint,
 PhoneNumber bigint,
 Email varchar(250)
 );
+select * from AddressBookTable ;
+----UC3 Insert New Contact-----
+insert into AddressBookTable values ('Vijay','Gavit','Nandurbar','Nandurbar','MH','425412','7648926432','viajgavit@gmail.com');
+insert into AddressBookTable values ('Tushar','patil','Dhule','Dhule','MH','567789','8934478393','tusharpatil@gmail.com'),('omkar','dhole','pune','pune','MH','764893','7874447733','omkardhole@gmail.com');
