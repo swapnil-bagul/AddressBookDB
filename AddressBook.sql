bd00fd1a00f098093e0be72b983b1f6f388db063
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-21 09:15:21 +0530

    [Swapnil] Added UC2 Create Table Of AddressBook

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,3 +1,14 @@
 ----UC1 create Database----
 create database AddressBook_Database;
 use AddressBook_Database;
+----UC2 Create Table------
+create table AddressBookTable(
+FirstName varchar(250),
+LastName varchar(250),
+Address varchar(250),
+City varchar(250),
+State varchar(250),
+Zip bigint,
+PhoneNumber bigint,
+Email varchar(250)
+);
